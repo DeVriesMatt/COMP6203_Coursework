717a070024ceec8a57dedd7f5279befb137600b0
De Hauwere Brent <brent.de.hauwere@student.ehb.be>
2019-11-30 19:28:56 +0000

    Pre-verified complete agent (excluding opponent model and elicitation).

---
+++ src/group1/OpponentModel.java
@@ -0,0 +1,27 @@
+package group1;
+
+import genius.core.Bid;
+
+/**
+ * Created by BrentDeHauwere on 30/11/2019.
+ */
+public class OpponentModel {
+    public OpponentModel(Bid bid) {
+
+    }
+
+    public static double hardheaded(int i) {
+        return 0;
+    }
+
+    public double getOpponentUtility(Bid bid) {
+        return 0;
+    }
+
+    public void addBid(Bid lastReceivedBid) {
+    }
+
+    public void addUtilityHistory(double lastReceivedBidUtility) {
+
+    }
+}
